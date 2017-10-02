224e2722aedfe8eeb64827c6227a54059d33035d
kevinsv97 <kevinsv97@gmail.com>
2017-10-02 15:52:15 -0500

    put package dao and interfaces to create example of DAO patron

---
+++ TIHC/src/java/dao/ConnectionDB.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+
+import java.sql.Statement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author jojstepersan
+ */
+public class ConnectionDB{
+    
+    private PreparedStatement insert;
+    private Statement statement;
+    private Connection conexion;
+    private ResultSet read;
+    
+    public ConnectionDB() 
+        {
+          try
+            {
+            Class.forName("com.mysql.jdbc.Driver");
+            conexion=DriverManager.getConnection("jdbc:mysql://localhost/tihc?user=root&password=12345");
+            statement=conexion.createStatement();    
+            }catch(ClassNotFoundException e)
+                {
+                System.out.println("no a puesto el diver en la libreria");
+                }
+            catch(SQLException sql)
+                {
+                System.out.println("recuerde que la contraseñña es: 12345 y es user root");
+                }
+        }
+    public static void main(String[] args) {
+        ConnectionDB c=new ConnectionDB();
+    }
+}
