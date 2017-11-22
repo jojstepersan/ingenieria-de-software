67447bfab9f5481c581fb80f82954731d6ef0480
IDEA-PC\Produccion <marioherrera0614@gmail.com>
2017-11-22 00:24:27 -0500

    Page's contract created! and it's ready to next step

---
+++ TIHC_final/src/java/Data/Contract.java
@@ -0,0 +1,80 @@
+
+package Data;
+
+
+public class Contract {
+    String nombre;
+    int identificacion;
+    String tipoCarga;
+    int cantidadPeso;
+    String origen;
+    String destino;
+    String descripcion;
+    int numeroRadicacion;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getIdentificacion() {
+        return identificacion;
+    }
+
+    public void setIdentificacion(int identificacion) {
+        this.identificacion = identificacion;
+    }
+
+    public String getTipoCarga() {
+        return tipoCarga;
+    }
+
+    public void setTipoCarga(String tipoCarga) {
+        this.tipoCarga = tipoCarga;
+    }
+
+    public int getCantidadPeso() {
+        return cantidadPeso;
+    }
+
+    public void setCantidadPeso(int cantidadPeso) {
+        this.cantidadPeso = cantidadPeso;
+    }
+
+    public String getOrigen() {
+        return origen;
+    }
+
+    public void setOrigen(String origen) {
+        this.origen = origen;
+    }
+
+    public String getDestino() {
+        return destino;
+    }
+
+    public void setDestino(String destino) {
+        this.destino = destino;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public int getNumeroRadicacion() {
+        return numeroRadicacion;
+    }
+
+    public void setNumeroRadicacion(int numeroRadicacion) {
+        this.numeroRadicacion = numeroRadicacion;
+    }
+    
+    
+}
